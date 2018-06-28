7f3748461cd32bad0b05186aa89488f7a48e45fe
Augustin Liebster <augustinliebster@Augustins-MacBook-Pro.local>
2018-06-28 11:51:12 -0500

    PartTwo

--- src/app/partners/partner/partner.component.ts
+++ src/app/partners/partner/partner.component.ts
@@ -10,6 +10,10 @@ import { UtilsService } from '../../utils/utils.service';
 
 import { MAT_DIALOG_DATA } from '@angular/material';
 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {BackendService} from '../../backend/backend.service';
+
+
 @Component({
   selector: 'app-partner',
   templateUrl: './partner.component.html',
@@ -23,9 +27,26 @@ export class PartnerComponent implements OnInit {
   public projects: Array<Object>;
   public students: Array<Object>;
 
+  public contactForm: FormGroup;
+
+  public contactResult= true;
+  public contactDisplay = false;
+
+
+
   public contacts = [{name: "FullName", phone: "Number", email:"email", title:"title", connected_parter:"connection"}];
 
+  private static objectToForm(object: Object) {
+    const formData: FormData = new FormData(); // Printing this out is weird be careful, use .get()
+    for (const key in object) {
+      formData.append(key, object[key]);
+    }
+
+    return formData;
+  }
+
   constructor(public partnerService: PartnerService,
+    private backendService: BackendService,
     private programsService: ProgramsService,
     @Inject(MAT_DIALOG_DATA) public id: string) {} // TODO: rename this id to be partnerName??
 
@@ -35,5 +56,39 @@ export class PartnerComponent implements OnInit {
       this.programs = programViews;
       this.programs.sort(UtilsService.comparisonFunction2);
     });
+
+
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      number: new FormControl(),
+      email: new FormControl(),
+      title: new FormControl(),
+      organization: new FormControl()
+    });
+
+
+
   }
+
+  public createContact() {
+    if (!this.contactForm.valid){
+      console.log('Form is not valid');
+    }
+    else{
+      console.log(this.contactForm.value)
+    }
+    // this.createEntity('contact', this.contactForm).then((entityResult: Object) => {
+    //   this.contactDisplay = true;
+    //   this.contactResult = entityResult;
+    // });
+  }
+
+
+//   public createEntity(type: string, form: FormGroup) {
+//     if (!form.valid) {
+//       console.log('Form is not valid'); // TODO: have this display
+//     } else {
+//       console.log('It works!')
+//   }
+// }
 }
