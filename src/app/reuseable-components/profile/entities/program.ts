33f7379cd3666d6732891e9c501327036629ed99
Augustin Liebster <augustinliebster@Augustins-MBP.attlocal.net>
2018-07-09 22:45:40 -0500

    Contacts

--- src/app/reuseable-components/profile/entities/program.ts
+++ src/app/reuseable-components/profile/entities/program.ts
@@ -59,11 +59,11 @@ export class ProgramBuilder extends EntityBuilder {
   public programName: string;
   public programPurpose: string;
   public programType: string;
-  public programNumStudents: int;
-  public programNumPartners: int;
-  public programPercentStudents: double;
-  public programPercentDidAnotherProgram: double;
-  public programPercentAcceptance: double;
+  public programNumStudents: number;
+  public programNumPartners: number;
+  public programPercentStudents: number;
+  public programPercentDidAnotherProgram: number;
+  public programPercentAcceptance: number;
 
   constructor() {
     super();
@@ -81,24 +81,24 @@ export class ProgramBuilder extends EntityBuilder {
     this.programType = pType;
   }
 
-  public numStudents(pNumStudents: int) {
+  public numStudents(pNumStudents: number) {
     this.programNumStudents = pNumStudents;
   }
 
-  public numPartners(pNumPartners: int) {
+  public numPartners(pNumPartners: number) {
     this.programNumPartners = pNumPartners;
   }
 
   // TODO: what does percent students mean
-  public percentStudents(pPercentStudents: double) {
+  public percentStudents(pPercentStudents: number) {
     this.programPercentStudents = pPercentStudents;
   }
 
-  public percentDidAnotherProgram(pPercentDidAnotherProgram: double) {
-    this.programPercentDidAnotherProgram = pPercenDidAnotherProgram;
+  public percentDidAnotherProgram(pPercentDidAnotherProgram: number) {
+    this.programPercentDidAnotherProgram = pPercentDidAnotherProgram;
   }
 
-  public percentAcceptance(pPercentAcceptance: double) {
+  public percentAcceptance(pPercentAcceptance: number) {
     this.programPercentAcceptance = pPercentAcceptance;
   }
 
